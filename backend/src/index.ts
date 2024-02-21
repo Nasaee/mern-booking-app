9aef97dabec78976fad33122bf815dd857bd342b
nasaee_window <nasaee.dev@gmail.com>
2024-02-21 23:46:50 +0700

    frontend create SignIn component, backend create velidate-token route and create verifyToken middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,12 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users.route';
 import authRoutes from './routes/auth.route';
+import cookiesParser from 'cookie-parser';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
-
+app.use(cookiesParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
